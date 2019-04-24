b23e6d2c94568f1228e554febe621a93dcc58b5c
Waves <creepykid1101@gmail.com>
2019-04-24 18:46:32 -0500

    escapes everyone and here now

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const settings = require('./jsons/settings.json');
 
+
+//empty
 exports.run = (bot, message, args, tools) => {
 message.delete()
 message.channel.send(args.join(' ').replace('@everyone', 'everyone').replace('@here', 'here'));
