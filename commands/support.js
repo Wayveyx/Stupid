7301b78aecb02af42d19de05050f1b1f4750aaee
Waves <creepykid1101@gmail.com>
2019-05-25 17:28:48 -0500

    added channel id

--- commands/support.js
+++ commands/support.js
@@ -10,7 +10,8 @@ const whmsg = new Webhook.MessageBuilder()
 .setName("Stupid Support")
 .setColor("#FFA500")
 .addField("User:", message.author.tag)
-.addField("Server:", `Name: ${message.guild.name}\nID: ${message.guild.id}`)
+.addField("Server:", `ID: ${message.guild.id}`)
+.addField("Channel:", `ID: ${message.channel.id}`)
 .addField("Message:", args.join(" ").replace("@", "(a)"))
 .setTime();
 hook.send(whmsg); //Im dumb lmao pls dont view previous versions
