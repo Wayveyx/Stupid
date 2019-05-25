0658a51b626a280ea22290b29ff2f84b603457e6
Waves <creepykid1101@gmail.com>
2019-05-25 05:11:01 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run = (bot, message, args, tools) => {
   .setTitle('Help Page')
   .setColor('#FFA500')
   .addField('Util Commands', '**sb;serverinfo** - Shows info about the server.\n**sb;userinfo** - Shows info about you or another person.\n**sb;botinfo** - Info about me.\n**sb;purge** - Purge 2-100 messages.\n**sb;ban** - Ban a user.\n**sb;kick** - Kick a user.')
-.addField('Misc. Commands', '**sb;request** - Request a command you think should be added.\n**sb;support** - Join the support server.\n**sb;invite** - Add me to your server.\n**sb;avatar** - My Avatar.\n**sb;myavatar** - Your avatar.\n**sb;sicon** - Server icon.')
+.addField('Misc. Commands', '**sb;request** - Request a command you think should be added.\n**sb;support** - Talk to the Support Server (One way).\n**sb;invite** - Add me to your server.\n**sb;avatar** - My Avatar.\n**sb;myavatar** - Your avatar.\n**sb;sicon** - Server icon.')
 .addField('Role commands', '**sb;role** - Custom roles. Role name must start with • and be below the bots role.\n**sb;color** - Colored roles. Role name must start with # and be below the the bots role.')
 .addField('Fun Commands', '**sb;cat** - Random cat (has api cooldown).\n**sb;say** - Make me say stuff.\n**sb;yomama** - Yo mama joke.\n**sb;8ball** - Magic 8 ball.\n**sb;gayrate** - See how gay someone is.\n**sb;waifu** - See how much of a waifu someone is')
 .setFooter('The help command is being redone and all commands may not be shown.'));                                                                                 //i want to die because of the 2 commands above this text  
