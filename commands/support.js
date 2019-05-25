313c896e2840bba270cf2862162f573448119b4e
Waves <creepykid1101@gmail.com>
2019-05-25 05:09:59 -0500

    Delete support.js

--- commands/support.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const settings = require('./jsons/settings.json');
-
-exports.run = (bot, message, args, tools) => {
-message.channel.send(new Discord.RichEmbed()
-.setTitle('Support Server')
-.setDescription('https://discord.gg/3e8KAZn')
-.setColor('#FFA500'));
-}
