9a44e08313fedf7940c680131f1e722884fec92e
Waves <creepykid1101@gmail.com>
2019-05-25 18:07:03 -0500

    ok

--- commands/reply.js
+++ commands/reply.js
@@ -3,5 +3,9 @@ const settings = require("./jsons/settings.json");
 
 exports.run = (bot, message, args) => {
 if(message.author.id !== settings.myid) return;
-bot.channels.get(args.join().slice(0, 18)).send(args.join(" ").slice(18));
+bot.channels.get(args.join().slice(0, 18).send(new Discord.RichEmbed()
+.setTitle("Support")
+.setColor("#FFA500")
+.setDescription(args.join(" ").slice(18))
+.setTimestamp();
 }
