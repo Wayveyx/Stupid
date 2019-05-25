04a5538448263684ee8de160fa1bd0311f899123
Waves <creepykid1101@gmail.com>
2019-05-25 17:35:26 -0500

    hmmmmmmm

---
+++ commands/reply.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const settings = require("./jsons/settings.json");
+
+exports.run = (bot, message, args) => {
+if(message.author.id !== settings.myid) return;
+try {
+bot.channels.get(args.join(" ").slice(0, 18)).send(args.slice(19));
+} catch(e) {
+message.channel.send(e.message);
+}
+}
