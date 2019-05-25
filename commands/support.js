6881547745483be5f60039c71e455c86ff16defc
Waves <creepykid1101@gmail.com>
2019-05-25 05:39:14 -0500

    webhooks r fun

--- commands/support.js
+++ commands/support.js
@@ -10,7 +10,7 @@ const whmsg = new Webhook.MessageBuilder()
 .setName("Stupid Support")
 .setColor("#FFA500")
 .addField("User:", message.author.tag)
-.addField("In Server:", `${message.guild.name} ${message.guild.id}`)
+.addField("Server:", `Name: ${message.guild.name}\nID: ${message.guild.id}`)
 .addField("Message:", args.join(" ").replace("@", "(a)"))
 .setTime();
 hook.send(whmsg); //Im dumb lmao pls dont view previous versions
