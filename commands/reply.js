9a1648044644986fa7d7e2c8e2bc14087bfad8ba
Waves <creepykid1101@gmail.com>
2019-05-25 18:00:14 -0500

    Update reply.js

--- commands/reply.js
+++ commands/reply.js
@@ -3,5 +3,6 @@ const settings = require("./jsons/settings.json");
 
 exports.run = (bot, message, args) => {
 if(message.author.id !== settings.myid) return;
+message.channel.send(args.join().slice(0, 1))
 bot.channels.get(args.join().slice(0, 1)).send(args.join(" ").slice(18));
 }
