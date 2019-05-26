24ff094c73bd9af927de6f9d7ce700ad6e745c5e
Waves <creepykid1101@gmail.com>
2019-05-25 23:39:48 -0500

    Ok this worked on the first try lmao

--- commands/free.js
+++ commands/free.js
@@ -3,6 +3,14 @@ const Discord = require("discord.js");
 const settings = require("./jsons/settings.json"); //spelling oops
 
 exports.run = async (bot, message, args) => {
-let sent = bot.channels.filter(m => m.name == "free-games").forEach(m => m.send(args.join(" "))); //Is this against TOS?
-message.channel.send("sent");
+if(!settings.ids.includes(message.author.id)) return;
+if(!message.content.toLowerCase().includes("https://")) return message.channel.send("Provide the link dummy.")
+if(message.content.toLowerCase().includes("porn")) return; //basic link filter ig
+let sent = await bot.channels.filter(m => m.name == "free-games").forEach(m => m.send(new Discord.RichEmbed()
+.setTitle("Free Game")
+.addField("Link:", args.join(" "))
+.setColor("#FFA500")
+.setTimestamp()));
+
+message.channel.send(`Link Sent to ${bot.channels.filter(m => m.name == "free-games").size} servers.`);
 }
