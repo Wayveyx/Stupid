8b458f0b0d6e0af2b7504c11bbb2f78bdcd1ed6c
Waves <creepykid1101@gmail.com>
2019-05-25 23:29:07 -0500

    fixed typos

--- commands/free.js
+++ commands/free.js
@@ -1,7 +1,8 @@
 //just letting others know when a game is free :)
 const Discord = require("discord.js");
-const settings = require("./jsons/settongs.jsom");
+const settings = require("./jsons/settings.json"); //spelling oops
 
 exports.run = async (bot, message, args) => {
-let sent = bot.channels.filter(m => m.name == "free-games").forEach(m => m.send(args.join(" "))) //Is this against TOS?
+let sent = bot.channels.filter(m => m.name == "free-games").forEach(m => m.send(args.join(" "))); //Is this against TOS?
+message.channel.send("sent");
 }
