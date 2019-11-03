f854c6d46ce2d2cb18490b9d725d014ba54f3375
Waves <creepykid1101@gmail.com>
2019-11-03 17:24:12 -0600

    e

--- pbot.js
+++ pbot.js
@@ -41,12 +41,10 @@ if(user.bot) return;
 if(message.channel.topic != null) {
 if(message.channel.topic.includes("Stupid:Disable")) return;
 }
-if (!msg.startsWith(pfix)) return;
-if(msg.includes("DISCORD")) {
 let regx = /^((?:https?:)?\/\/)?((?:www|m)\.)? ((?:discord\.gg|discordapp\.com))/g
 let check = regx.test(message.content.toLowerCase().replace(/\s+/g, ''))
 if(check.includes("discord")) return message.channel.send("yes");
-}
+if (!msg.startsWith(pfix)) return;
 try{
 let commandFile = require(`./commands/${cmd}.js`);
 commandFile.run(bot, message, args);
